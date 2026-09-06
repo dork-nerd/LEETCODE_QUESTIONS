63cca815c1a9449af0a64bcd29bd1c2588b893ba
MOHIT GUPTA <mgpersonal1718@gmail.com>
2026-09-06 23:42:39 +0530

    Time: 12 ms (86.09%) | Memory: 28.4 MB (64.23%) - LeetSync

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
@@ -2,14 +2,10 @@
 class Solution {
 public:
     string removeStars(string s) {
-        vector<char> vec;
         string str = "";
         rep(i,0,s.size()){
-            if(s[i]>='a' && s[i]<='z') vec.push_back(s[i]);
-            else vec.pop_back();
-        }
-        rep(i,0,vec.size()){
-            str += vec[i];
+            if(s[i]>='a' && s[i]<='z') str.push_back(s[i]);
+            else str.pop_back();
         }
         return str;
     }
